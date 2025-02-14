d5422d031713938922bda322508f0cfdce84c207
adityagohad <adityagohad@gmail.com>
2025-02-14 13:21:11 +0530

    code cleanup

--- kotak_demo/android/app/src/main/kotlin/com/example/kotak_demo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kotak_demo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- sahi_demo/android/app/src/main/kotlin/com/example/sahi_demo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sahi_demo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
