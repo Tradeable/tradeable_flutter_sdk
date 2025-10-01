e6bacd5fe059521e5dfe930eea0bcbaab1471e59
adityagohad <adityagohad@gmail.com>
2025-10-01 14:24:46 +0530

    added encyption method

--- lib/src/network/axis_api.dart
+++ lib/src/network/axis_api.dart
@@ -14,9 +14,9 @@ Future<String> axisHandshake() async {
     "https://custauth-apigee.uat.asldt.com/handshake",
     options: Options(
       headers: {
-        'x-api-client-id': 'ASLTRADER3',
+        'x-api-client-id': '',
         'content-type': 'application/json',
-        'Authorization': '2u1h5NWkPZxP8L42bzCHLLIBP2VLcZ9RhhFoLLcAScfVNW1K',
+        'Authorization': '',
       },
     ),
   );
