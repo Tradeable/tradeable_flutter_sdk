3410f659775554f7624ba57c784ce221cce5cd75
adityagohad <adityagohad@gmail.com>
2025-10-08 15:38:27 +0530

    deleted widgets

--- lib/src/ui/pages/recipe_page.dart
+++ lib/src/ui/pages/recipe_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tradeable_flutter_sdk/src/models/user_widgets_model.dart';
-import 'package:tradeable_flutter_sdk/tradeable_flutter_sdk.dart';
+import 'package:tradeable_flutter_sdk/src/network/api.dart';
 import 'package:tradeable_learn_widget/tradeable_learn_widget.dart';
 import 'package:tradeable_learn_widget/user_story_widget/models/user_story_model.dart';
 import 'package:tradeable_learn_widget/buy_sell_widget/buy_sell.dart';
