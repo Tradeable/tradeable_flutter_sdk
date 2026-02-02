d4e191b6d185211ca1eaa69d56972bab253dfe5d
adityagohad <adityagohad@gmail.com>
2026-02-02 11:59:09 +0530

    event name fix

--- lib/src/utils/events.dart
+++ lib/src/utils/events.dart
@@ -2,18 +2,19 @@ class AppEvents {
   static const String viewAllOverallProgress =
       'TRADEABLE_USER_CLICKED_VIEW_ALL_OVERALL_PROGRESS';
 
-  static const String viewAllCourses = 'USER_CLICKED_VIEW_ALL_COURSES';
+  static const String viewAllCourses =
+      'TRADEABLE_USER_CLICKED_VIEW_ALL_COURSES';
 
   static const String viewAllTopicsInCourse =
-      'TRADEABLEUSER_OPENED_VIEW_ALL_TOPICS_IN_COURSE';
+      'TRADEABLE_USER_OPENED_VIEW_ALL_TOPICS_IN_COURSE';
 
   static const String courseBottomSheetOpened =
-      "TRADEABLEUSER_OPENED_COURSE_BOTTOM_SHEET";
+      "TRADEABLE_USER_OPENED_COURSE_BOTTOM_SHEET";
 
-  static const String beginTopic = "TRADEABLEUSER_CLICKED_BEGIN_TOPIC";
+  static const String beginTopic = "TRADEABLE_USER_CLICKED_BEGIN_TOPIC";
 
-  static const String quickLinkClick = "TRADEABLEUSER_CLICKED_QUICK_LINK";
+  static const String quickLinkClick = "TRADEABLE_USER_CLICKED_QUICK_LINK";
 
   static const String learnDashboardBannerClick =
-      'TRADEABLEUSER_CLICKED_LEARN_DASHBOARD_BANNER';
+      'TRADEABLE_USER_CLICKED_LEARN_DASHBOARD_BANNER';
 }
